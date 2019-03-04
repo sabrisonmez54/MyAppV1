a4b749f3ca731e5e2411a5fd494e64d669b9096b
Sabri <sabri.sonmez@macaulay.cuny.edu>
2019-03-04 01:21:40 -0500

    Initial Commit

---
+++ NewMyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewMyApp
+//
+//  Created by Sabri Sönmez on 3/4/19.
+//  Copyright © 2019 Sabri Sönmez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

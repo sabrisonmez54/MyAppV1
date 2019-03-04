ff460e3aa302861d510a5b8961b308f4bef00758
Sabri <sabri.sonmez@macaulay.cuny.edu>
2019-03-04 10:42:24 -0500

    scroll view implementation

--- NewMyApp/ViewController.swift
+++ NewMyApp/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var profileIconView: UIView!
+    @IBOutlet weak var cardView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+           scrollView.contentSize = CGSize(width: 414, height: 1276)
         // Do any additional setup after the view
+        titleLbl.alpha = 0
+        
+        UIView.animate(withDuration: 1){
+            self.titleLbl.alpha = 1
+        }
+        
     }
+    
+    
+    
     
 
 }

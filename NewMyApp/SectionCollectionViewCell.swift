d2c481b3b53ea9489d1252ecd3a6a1090586ed41
Sabri <sabri.sonmez@macaulay.cuny.edu>
2019-03-04 18:25:10 -0500

    moving onto VCs

---
+++ NewMyApp/SectionCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SectionCollectionViewCell.swift
+//  NewMyApp
+//
+//  Created by Sabri Sönmez on 3/4/19.
+//  Copyright © 2019 Sabri Sönmez. All rights reserved.
+//
+
+import UIKit
+
+class SectionCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var coverImageView: UIImageView!
+    @IBOutlet weak var cellCaptionLbl: UILabel!
+    @IBOutlet weak var cellTitleLbl: UILabel!
+
+
+
+}
--- NewMyApp/ViewController.swift
+++ NewMyApp/ViewController.swift
@@ -16,9 +16,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var mainView: UIView!
     @IBOutlet weak var profileIconView: UIView!
     @IBOutlet weak var cardView: UIView!
-    
     @IBOutlet weak var cardCollectionView: UICollectionView!
     
+    var titles = ["Profile","GitHub","Resume","Linkedin"]
+    var captions = ["A little bit about me.","This is my GitHub account where I upload all my projects.","Here lies my resume","My Linkedin profile "]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,16 +46,17 @@ extension ViewController : UICollectionViewDelegate , UICollectionViewDataSource
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sectionCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sectionCell", for: indexPath) as! SectionCollectionViewCell
+        
+        cell.cellTitleLbl.text = titles[indexPath.row]
+        cell.cellCaptionLbl.text = captions[indexPath.row]
         
         return cell
     }
-    
-    
-    
 }
 
 extension ViewController : UIScrollViewDelegate{
+    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let OffsetY = scrollView.contentOffset.y
         
@@ -62,6 +64,23 @@ extension ViewController : UIScrollViewDelegate{
             mainView.transform = CGAffineTransform(translationX: 0, y: OffsetY)
             titleLbl.transform = CGAffineTransform(translationX: 0, y: -OffsetY/4)
             backgroundImage.transform = CGAffineTransform(translationX: 0, y: -OffsetY/5)
+        }
+        if let collectionView = scrollView as? UICollectionView {
+            for cell in collectionView.visibleCells as! [SectionCollectionViewCell]{
+                let indexPath = collectionView.indexPath(for: cell)!
+                let attributes = collectionView.layoutAttributesForItem(at: indexPath)!
+                let cellFrame = collectionView.convert(attributes.frame, to: view)
+                let translationX = cellFrame.origin.x / 5
+                cell.coverImageView.transform = CGAffineTransform(translationX: translationX, y: 0)
+                let angleFromX = Double((-cellFrame.origin.x)/10)
+                let angle = CGFloat(angleFromX * Double.pi / 180.0)
+              
+                var transform = CATransform3DIdentity
+                transform.m34 = -1.0 / 1000
+                 //rotate with perspective
+                let rotation = CATransform3DRotate(transform, angle, 0, 1 , 0)
+                cell.layer.transform = rotation
+            }
         }
     }
     

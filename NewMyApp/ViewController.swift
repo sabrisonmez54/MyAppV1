6de1ba79858416ba1f5437e765614b7dde55188b
Sabri <sabri.sonmez@macaulay.cuny.edu>
2019-03-04 11:43:26 -0500

    cards added

--- NewMyApp/ViewController.swift
+++ NewMyApp/ViewController.swift
@@ -17,21 +17,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var profileIconView: UIView!
     @IBOutlet weak var cardView: UIView!
     
+    @IBOutlet weak var cardCollectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-           scrollView.contentSize = CGSize(width: 414, height: 1276)
-        // Do any additional setup after the view
-        titleLbl.alpha = 0
         
+        cardCollectionView.delegate = self
+        cardCollectionView.dataSource = self
+        
+        scrollView.contentSize = CGSize(width: 414, height: 1276)
+        scrollView.delegate = self
+        
+        titleLbl.alpha = 0
+       
         UIView.animate(withDuration: 1){
             self.titleLbl.alpha = 1
         }
         
     }
-    
-    
-    
-    
+}
 
+extension ViewController : UICollectionViewDelegate , UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sectionCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+    
+}
+
+extension ViewController : UIScrollViewDelegate{
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let OffsetY = scrollView.contentOffset.y
+        
+        if OffsetY < 0{
+            mainView.transform = CGAffineTransform(translationX: 0, y: OffsetY)
+            titleLbl.transform = CGAffineTransform(translationX: 0, y: -OffsetY/4)
+            backgroundImage.transform = CGAffineTransform(translationX: 0, y: -OffsetY/5)
+        }
+    }
+    
+    
 }
 
